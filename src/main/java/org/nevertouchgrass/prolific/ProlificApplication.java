da08b908b028ea07313d0841c682fa7080daa6a3
Anton Horobets <anton.horobets@erstegroup.com>
2025-02-21 17:58:55 +0100

    init

---
+++ src/main/java/org/nevertouchgrass/prolific/ProlificApplication.java
@@ -0,0 +1,13 @@
+package org.nevertouchgrass.prolific;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProlificApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProlificApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nevertouchgrass/prolific/ProlificApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nevertouchgrass.prolific;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProlificApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
