dbf5085d087537c71c91ee1ba39af8085378267f
ikvict07 <antongorobec101@gmail.com>
2025-02-24 14:51:52 +0100

    fix if run from idea

--- src/main/java/org/nevertouchgrass/prolific/service/PathService.java
+++ src/main/java/org/nevertouchgrass/prolific/service/PathService.java
@@ -1,22 +1,24 @@
 package org.nevertouchgrass.prolific.service;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.NoSuchElementException;
 
 @Service
+@Log4j2
 public class PathService {
-
     public Path getProjectPath() {
         Class<?> clazz = PathService.class;
         URL classResource = clazz.getResource(clazz.getSimpleName() + ".class");
         if (classResource == null) {
-            throw new RuntimeException("class resource is null");
+            throw new IllegalStateException("class resource is null");
         }
         String url = classResource.toString();
-        System.out.println("URL is: " + url);
+        log.info("Working in directory: {}", url);
         if (url.startsWith("jar:")) {
             String fixed = url
                     .replace("jar:", "")
@@ -24,11 +26,21 @@ public class PathService {
                     .replace("nested:", "");
             int index = fixed.indexOf(".jar");
             if (index == -1) {
-                throw new RuntimeException("Invalid Jar File URL String");
+                throw new NoSuchElementException("Invalid Jar File URL String");
             }
             String path = fixed.substring(0, index + 4);
             return Paths.get(path);
         }
-        throw new RuntimeException("Invalid Jar File URL String");
+        if (url.startsWith("file:")) {
+            String fixed = url.replace("file:", "");
+            int index = fixed.indexOf("/build");
+            if (index == -1) {
+                throw new NoSuchElementException("Invalid Jar File URL String");
+            }
+            String path = fixed.substring(0, index);
+            log.info("Working in directory: {}", path);
+            return Paths.get(path);
+        }
+        throw new IllegalStateException("Invalid Jar File URL String");
     }
 }
