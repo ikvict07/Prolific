2b4641b26710b73fe8b37dade6ec7f53c302e62e
Dzmitry Petukhou <petukhou.dzmitry@gmail.com>
2025-02-27 21:16:55 +0100

    Refactor ProjectScannerService to improve directory scanning: utilize Files.walkFileTree for better readability and performance, and consolidate path matching logic

--- src/main/java/org/nevertouchgrass/prolific/service/ProjectScannerService.java
+++ src/main/java/org/nevertouchgrass/prolific/service/ProjectScannerService.java
@@ -23,10 +23,18 @@ public class ProjectScannerService {
 
     private final List<ProjectTypeModel> projectTypeModels;
     private final Set<Path> projects = ConcurrentHashMap.newKeySet();
+    private final PathMatcher pathMatcher;
 
     @Autowired
     public ProjectScannerService(@NonNull XmlProjectScannerConfigLoaderService configLoaderService) {
         this.projectTypeModels = configLoaderService.loadProjectTypes();
+
+        List<String> matchers = new ArrayList<>();
+        for (ProjectTypeModel projectTypeModel : projectTypeModels) {
+            matchers.addAll(projectTypeModel.getIdentifiers());
+        }
+        String pattern = String.format("glob:**/{%s}", String.join(",", matchers));
+        this.pathMatcher = FileSystems.getDefault().getPathMatcher(pattern);
     }
 
     public Set<Path> scanForProjects(String rootDirectory) {
@@ -37,52 +45,70 @@ public class ProjectScannerService {
 
     private class FileVisitorTask extends RecursiveTask<Set<Path>> {
         private final Path path;
-        private final PathMatcher pathMatcher;
 
         private FileVisitorTask(Path path) {
             this.path = path;
-            List<String> matchers = new ArrayList<>();
-            for (ProjectTypeModel projectTypeModel : projectTypeModels) {
-                matchers.addAll(projectTypeModel.getIdentifiers());
-            }
-            String pattern = String.format("glob:**/{%s}", String.join(",", matchers));
-            this.pathMatcher = FileSystems.getDefault().getPathMatcher(pattern);
         }
 
         @Override
         protected Set<Path> compute() {
             try {
                 List<FileVisitorTask> fileVisitorTasks = new ArrayList<>();
-                try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path,
-                        entry -> Files.isReadable(entry) &&
-                                !Files.isSymbolicLink(entry) &&
-                                Files.exists(entry)
-                )) {
-                    for (Path entry : directoryStream) {
-                        if (!Files.readSymbolicLink(path).equals(path)) {
-                            continue;
+
+                Files.walkFileTree(path, new FileVisitor<>() {
+                    @Override
+                    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+                        if (Files.isReadable(dir)) {
+                            if (pathMatcher.matches(dir)) {
+                                addProject(dir);
+                                return FileVisitResult.SKIP_SUBTREE;
+                            }
+
+                            if (!dir.equals(path)) {
+                                FileVisitorTask fileVisitorTask = new FileVisitorTask(dir);
+                                fileVisitorTask.fork();
+                                fileVisitorTasks.add(fileVisitorTask);
+                                return FileVisitResult.SKIP_SUBTREE;
+                            }
+                            return FileVisitResult.CONTINUE;
                         }
-                        if (Files.isDirectory(entry)) {
-                            if (pathMatcher.matches(entry)) {
-                                projects.add(entry.getParent().toRealPath(LinkOption.NOFOLLOW_LINKS));
-                                break;
+
+                        return FileVisitResult.SKIP_SUBTREE;
+                    }
+
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                        if(Files.isReadable(file)) {
+                            if (pathMatcher.matches(file)) {
+                                addProject(file);
+                                return FileVisitResult.SKIP_SIBLINGS;
                             }
-                            FileVisitorTask fileVisitorTask = new FileVisitorTask(entry);
-                            fileVisitorTask.fork();
-                            fileVisitorTasks.add(fileVisitorTask);
-                        } else {
-                            if (pathMatcher.matches(entry)) {
-                                projects.add(entry.getParent().toRealPath(LinkOption.NOFOLLOW_LINKS));
-                                break;
-                            }
+                        }
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                        log.error("{}", exc.getMessage());
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+                        for (FileVisitorTask fileVisitorTask : fileVisitorTasks) {
+                            projects.addAll(fileVisitorTask.join());
+                        }
+                        return FileVisitResult.SKIP_SUBTREE;
+                    }
+
+                    private void addProject(Path path) {
+                        try {
+                            projects.add(path.getParent().toRealPath(LinkOption.NOFOLLOW_LINKS));
+                        } catch (IOException e) {
+                            log.error("{}", e.getMessage());
                         }
                     }
-                } catch (IOException e) {
-                    log.error("{}", e.getMessage());
-                }
-                for (FileVisitorTask fileVisitorTask : fileVisitorTasks) {
-                    projects.addAll(fileVisitorTask.join());
-                }
+                });
             } catch (Exception e) {
                 log.error("{}", e.getMessage());
             }
