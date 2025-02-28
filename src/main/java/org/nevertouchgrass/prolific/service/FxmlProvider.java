5fc54dbb70d006105b5c16f33c9646c4494c88a5
Dzmitry Petukhou <petukhou.dzmitry@gmail.com>
2025-02-28 14:17:04 +0100

    Refactor FXML and CSS files for improved layout and styling; remove unnecessary annotations in FxmlProvider

--- src/main/java/org/nevertouchgrass/prolific/service/FxmlProvider.java
+++ src/main/java/org/nevertouchgrass/prolific/service/FxmlProvider.java
@@ -1,9 +1,7 @@
 package org.nevertouchgrass.prolific.service;
 
-import lombok.SneakyThrows;
 import org.nevertouchgrass.prolific.model.FxmlLoadedResource;
 
 public interface FxmlProvider {
-    @SneakyThrows
     <T> FxmlLoadedResource<T> getFxmlResource(String parentName);
 }
