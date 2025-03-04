34422fd6210ecb1a28c967709b4772b27e6dcd8c
ikvict07 <antongorobec101@gmail.com>
2025-03-04 14:04:11 +0100

    PathService rework and tests

--- src/main/java/org/nevertouchgrass/prolific/service/PathService.java
+++ src/main/java/org/nevertouchgrass/prolific/service/PathService.java
@@ -1,12 +1,14 @@
 package org.nevertouchgrass.prolific.service;
 
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.NoSuchElementException;
+import java.util.Objects;
 
 /**
  * Service that manages paths
@@ -14,48 +16,29 @@ import java.util.NoSuchElementException;
 @Service
 @Log4j2
 public class PathService {
+    @SneakyThrows
     public Path getProjectPath() {
         Class<?> clazz = PathService.class;
-        URL classResource = clazz.getResource(clazz.getSimpleName() + ".class");
-        if (classResource == null) {
-            throw new IllegalStateException("class resource is null");
-        }
-        String url = classResource.toString();
-        log.info("Working in directory: {}", url);
-        if (url.startsWith("jar:")) {
-            String fixed = url
-                    .replace("jar:", "")
-                    .replace("file:", "")
-                    .replace("nested:", "");
-            int index = fixed.indexOf(".jar");
-            if (index == -1) {
-                throw new NoSuchElementException("Invalid Jar File URL String");
-            }
-            String path;
-            if (System.getProperty("os.name").toLowerCase().contains("win")) {
-                path = fixed.substring(1, index + 4);
-
-            } else {
-                path = fixed.substring(0, index + 4);
-
-            }
+        URI classResource = URI.create(Objects.requireNonNull(clazz.getResource(clazz.getSimpleName() + ".class")).getPath());
+        log.info("Working in directory: {}", classResource);
+        return normalizeUrl(classResource);
+    }
+    @SneakyThrows
+    public Path normalizeUrl(URI uri) {
+        String url = uri.getSchemeSpecificPart();
+        var offset =System.getProperty("os.name").toLowerCase().contains("win") ? 1 : 0;
+        url = url.substring(offset);
+        if (url.contains(".jar")) {
+            int index = url.indexOf(".jar");
+            String path = url.substring(0, index + 4);
             return Paths.get(path);
         }
-        if (url.startsWith("file:")) {
-            String fixed = url.replace("file:", "");
-            int index = fixed.indexOf("/build");
-            if (index == -1) {
-                throw new NoSuchElementException("Invalid Jar File URL String");
-            }
-            String path;
-            if (System.getProperty("os.name").toLowerCase().contains("win")) {
-                path = fixed.substring(1, index);
-            } else {
-                path = fixed.substring(0, index);
-            }
+        int index = url.indexOf("/build");
+        if (index != -1) {
+            String path = url.substring(0, index);
             log.info("Working in directory: {}", path);
             return Paths.get(path);
         }
-        throw new IllegalStateException("Invalid Jar File URL String");
+        return Paths.get(url);
     }
 }
