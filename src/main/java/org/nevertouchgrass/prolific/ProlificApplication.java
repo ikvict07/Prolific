0cb27221ed731010b69189c47d1c11d31c5435eb
ikvict07 <antongorobec101@gmail.com>
2025-03-10 02:19:06 +0100

    db path change

--- src/main/java/org/nevertouchgrass/prolific/ProlificApplication.java
+++ src/main/java/org/nevertouchgrass/prolific/ProlificApplication.java
@@ -1,9 +1,13 @@
 package org.nevertouchgrass.prolific;
 
+import lombok.SneakyThrows;
 import org.nevertouchgrass.prolific.listener.JavaFxRuntimeInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 
 /**
@@ -12,7 +16,12 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 public class ProlificApplication {
+    @SneakyThrows
     public static void main(String[] args) {
+        String userHome = System.getProperty("user.home");
+        String dbPath = userHome + "/Prolific/data/prolific.sqlite";
+        Files.createDirectories(Path.of(dbPath).getParent());
+        System.setProperty("spring.datasource.url", "jdbc:sqlite:" + dbPath);
         var appBuilder = new SpringApplicationBuilder(ProlificApplication.class);
         appBuilder.headless(false);
         var app = appBuilder.build();
