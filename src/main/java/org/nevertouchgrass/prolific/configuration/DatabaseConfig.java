45c5dafc443f857c5b63b86dfc25c938a5378508
ikvict07 <antongorobec101@gmail.com>
2025-03-13 22:36:40 +0100

    DB path change

--- src/main/java/org/nevertouchgrass/prolific/configuration/DatabaseConfig.java
+++ src/main/java/org/nevertouchgrass/prolific/configuration/DatabaseConfig.java
@@ -2,20 +2,24 @@ package org.nevertouchgrass.prolific.configuration;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Configuration
 public class DatabaseConfig {
-    @Value("${spring.datasource.url}")
-    private String url;
-
     @Bean
+    @SneakyThrows
     public DataSource dataSource() {
         HikariConfig hikariConfig = new HikariConfig();
+        String userHome = System.getProperty("user.home");
+        String dbPath = userHome + "/Prolific/data/prolific.sqlite";
+        Files.createDirectories(Path.of(dbPath).getParent());
+        var url = "jdbc:sqlite:" + dbPath;
         hikariConfig.setJdbcUrl(url);
         hikariConfig.setDriverClassName("org.sqlite.JDBC");
         hikariConfig.setMaximumPoolSize(10);
