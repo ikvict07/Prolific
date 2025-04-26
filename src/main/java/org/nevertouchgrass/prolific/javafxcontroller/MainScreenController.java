7e0eb49387050b52dfc6ea29e1112f97cc518f0a
Dzmitry Petukhou <petukhou.dzmitry@gmail.com>
2025-04-26 03:18:21 +0200

    Refactor header component for OS-specific rendering and update font size in styles

--- src/main/java/org/nevertouchgrass/prolific/javafxcontroller/MainScreenController.java
+++ src/main/java/org/nevertouchgrass/prolific/javafxcontroller/MainScreenController.java
@@ -3,12 +3,14 @@ package org.nevertouchgrass.prolific.javafxcontroller;
 import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.nevertouchgrass.prolific.annotation.Initialize;
 import org.nevertouchgrass.prolific.annotation.StageComponent;
+import org.nevertouchgrass.prolific.service.FxmlProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Lazy;
@@ -20,14 +22,31 @@ import org.springframework.context.annotation.Lazy;
 public class MainScreenController {
     @Setter(onMethod_ = {@Qualifier("primaryStage"), @Autowired})
     private Stage stage;
+    @Setter(onMethod_ = @Autowired)
+    private FxmlProvider fxmlProvider;
 
     @FXML private AnchorPane mainScreen;
     @FXML private VBox logsAndMetricsPanel;
     @FXML private AnchorPane footer;
     @FXML private VBox projectsPanel;
+    @FXML private StackPane header;
+
+    @FXML private AnchorPane headerMac;
+    @FXML private AnchorPane headerLinux;
 
     @Initialize
     public void init() {
+        String os = System.getProperty("os.name").toLowerCase();
+
+        if (os.contains("mac")) {
+            header.getChildren().add(headerMac);
+        } else if (os.contains("linux")) {
+            header.getChildren().add(headerLinux);
+        } else {
+            log.warn("Unsupported OS: {}", os);
+            throw new RuntimeException("Unsupported OS: " + os);
+        }
+
         ChangeListener<Number> block1 = (observable, oldValue, newValue) -> mainScreen.setPrefWidth(newValue.doubleValue());
         ChangeListener<Number> block2 = (observable, oldValue, newValue) -> mainScreen.setPrefHeight(newValue.doubleValue());
         stage.widthProperty().addListener(block1);
