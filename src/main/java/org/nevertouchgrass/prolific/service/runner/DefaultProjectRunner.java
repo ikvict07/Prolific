37b987faeee086f16107a480bca9320f3b16bc75
ikvict07 <antongorobec101@gmail.com>
2025-04-27 01:37:32 +0200

    No metrics user

--- src/main/java/org/nevertouchgrass/prolific/service/runner/DefaultProjectRunner.java
+++ src/main/java/org/nevertouchgrass/prolific/service/runner/DefaultProjectRunner.java
@@ -2,11 +2,13 @@ package org.nevertouchgrass.prolific.service.runner;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.nevertouchgrass.prolific.constants.action.SeeMetricsAction;
 import org.nevertouchgrass.prolific.exception.ProcessStartFailedException;
 import org.nevertouchgrass.prolific.model.Project;
 import org.nevertouchgrass.prolific.model.RunConfig;
 import org.nevertouchgrass.prolific.service.logging.ProcessLogsService;
 import org.nevertouchgrass.prolific.service.metrics.MetricsService;
+import org.nevertouchgrass.prolific.service.permissions.PermissionRegistry;
 import org.nevertouchgrass.prolific.service.runner.contract.ProjectRunner;
 import org.nevertouchgrass.prolific.util.ProcessWrapper;
 
@@ -21,6 +23,7 @@ public abstract class DefaultProjectRunner implements ProjectRunner {
 
     private final ProcessLogsService processLogsService;
     private final MetricsService metricsService;
+    private final PermissionRegistry permissionRegistry;
 
     public abstract String getType();
 
@@ -35,7 +38,9 @@ public abstract class DefaultProjectRunner implements ProjectRunner {
             procWrapper = ProcessWrapper.of(process);
             procWrapper.setName(runConfig.getConfigName());
             processLogsService.observeProcess(procWrapper);
-            metricsService.observeProcess(procWrapper);
+            if (permissionRegistry.getChecker(SeeMetricsAction.class).hasPermission(new SeeMetricsAction())) {
+                metricsService.observeProcess(procWrapper);
+            }
             process.onExit()
                     .thenAccept(_ -> {
                         var descendantsToReceive = new HashSet<ProcessHandle>();
